b51b75ca68083f20c037b5565d43c5d47e2807c9
SOURABHSAHU03 <144714122+SOURABHSAHU03@users.noreply.github.com>
2024-01-26 17:40:27 +0000

    variables

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 15520
+let accountEmail = "jlsdji@google.com"
+var accountPassword = "123654"
+accountCity = "jaipur"
+let accountstate;
+// accountId = 2 not allowed
+accountEmail = "jsdkh.com"
+accountPassword = "3213"
+accountCity = "dhkjf"
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
