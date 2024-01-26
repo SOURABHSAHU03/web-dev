2dd13f5332cc9ddd01e8a44d1257f273c7de3814
SOURABHSAHU03 <144714122+SOURABHSAHU03@users.noreply.github.com>
2024-01-26 18:28:27 +0000

    datatypes

---
+++ 02_basic/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) we are using node js not browser
+
+let name = "goldy"
+let age = 20
+let isloogedIn = false
+let state = null
+
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true or false 
+//null => standalone value // empty
+//undefined => 
+//symbol => to find uniqueness 
+
+//object 
+
+
+console.log(typeof"kjdhfk");
+console.log(typeof null);
+console.log(typeof undefined);
